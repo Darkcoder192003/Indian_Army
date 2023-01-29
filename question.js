1b51bc7922e3cf85f37da94ac5aecf23e54add50
Darkcoder192003 <100104465+Darkcoder192003@users.noreply.github.com>
2023-01-29 18:00:10 +0530

    uploaded all files

---
+++ question.js
@@ -0,0 +1,126 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When is the Army Day celebrated in India every year?",
+    answer: "January 15",
+    options: [
+      "January 15",
+      "January 10",
+      "January 12",
+      "December 4"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Why January 15 is Celebrated as the Army Day in India?",
+    answer: "Field Marshal K.M. Cariaapa took over as the first Inian Commander-in-Chief of the Indian Army",
+    options: [
+      "India won the war against Pakistan",
+      "Birth anniversary of Field Marshal K.M. Cariappa",
+      "Indian Rebellion of 1857 began on this day",
+      "Field Marshal K.M. Cariaapa took over as the first Inian Commander-in-Chief of the Indian Army"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which is the highest rank of the Indian Army?",
+    answer: "Field Marshal",
+    options: [
+      "Brigadier",
+      "General",
+      "Field Marshal",
+      "Major"
+    ]
+  },
+    {
+    numb: 4,
+    question: "When was the first war between ndi and Pakistan took place?",
+    answer: "1947",
+    options: [
+      "1947",
+      "1965",
+      "1999",
+      "2005"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who was the last British Commander-in-Chief of the Indian Army?",
+    answer: "General Francis  Butcher",
+    options: [
+      "General Rob Lockhart",
+      "General Francis  Butcher",
+      "General Frank Messervy",
+      "General Alan Hartley"
+    ]
+  },
+    {
+    numb: 6,
+    question: "When was the Indian Army founded?",
+    answer: "1 April 1895",
+    options: [
+      "15 August 1947",
+      "26 January 1950",
+      "1 April 1895",
+      "15 January 1857"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Who was the first Field Marshal the Indian Army?",
+    answer: "Sam Manekshaw",
+    options: [
+      "Nirmal Chandra Vij",
+      "Arjan Singh",
+      "Kodandera M. Cariappa",
+      "Sam Manekshaw"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is the smallest unit of the Indian Army called?",
+    answer: "Section",
+    options: [
+      "Platoon",
+      "Section",
+      "Battery",
+      "Battalion"
+    ]
+  },
+  {
+    numb: 9,
+    question: "The PIN code of the Army Post Office starts with?",
+    answer: "9",
+    options: [
+      "1",
+      "7",
+      "8",
+      "9"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Who was the first recipient of the Param Vir Chakra?",
+    answer: "Major Somnath Sharma",
+    options: [
+      "Major Dhan Singh Thapa",
+      "Major Shaitan Singh",
+      "Major Somnath Sharma",
+      "Major Hoshiar Singh"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
